953b70b5667556fed00c7f5c9e76ba2611a4ddff
Jaeyun Jung <rangyun36@gmail.com>
2022-02-16 15:13:24 +0900

    fix(test): change index.html page

---
+++ now/src/COMPONENTS/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Intro() {
+  return (
+    <div style={{textAlign:"center"}}>
+      <h1>now 프로젝트</h1>
+      <br/>
+      <p>사람들의 시간을 줄여주는 서비스를 개발을 위한 아이디어 플랫폼</p>
+    </div>
+  )
+}
